29014f46e98312e2bc2bb0b905d40abc01cae127
ButtonUp! <EFox2413@gmail.com>
2016-03-23 17:27:42 +0700

    Create weather-script.js

---
+++ weather-script.js
@@ -0,0 +1,84 @@
+// ==UserScript==
+// @name         WeatherScript
+// @namespace    https://github.com/EFox2413/initiumGrease
+// @version      0.1
+// @description  try to take over the world!
+// @author       EFox2413
+// @match       https://www.playinitium.com/*
+// @grant        none
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+var $ = window.jQuery; 
+
+// Weather ratio from getWeather function in server js file...
+var weatherInt = getWeather();
+var lightning = getLightning();
+var nextWeatherInt = getNextWeather();
+var nextLightning = getNextLightning();
+
+function translateToString(wRatio, LRatio) {
+    if (wRatio > 0.5) {
+        if ( LRatio > 0) {
+            return "Stormy";
+        } else {
+        return"Rain";
+		}
+    } else if (wRatio <0.5) {
+        return "Sunny";
+    }
+    return "N/A";
+}
+
+$( '.header-location' ).append(  '<span> ' + "       Now: " +  translateToString(weatherInt, lightning) + '<//span>' );
+$( '.header-location' ).append(  '<span> ' + "       Next: " +  translateToString(nextWeatherInt, nextLightning) + '<//span>' );
+
+
+
+// Weather calculator
+function getNextWeather()
+{
+	// <%=GameUtils.getWeather()%>
+	var serverTime = getCurrentServerTime();
+	
+	var date = new Date(serverTime);
+	
+	var behindHour = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours()+1);
+	var aheadHour = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours()+2);
+	var behindMs = behindHour.getTime();
+	var aheadMs = aheadHour.getTime();
+
+	
+	var behindHourWeather = rnd((behindMs/3600000), 0, 1);
+	var aheadHourWeather = rnd((aheadMs/3600000), 0, 1);
+	
+	// Now interpolate...
+	var weatherDifference = aheadHourWeather-behindHourWeather;
+	
+	var hourProgression = (serverTime-behindHour)/3600000;
+	
+	var interpolationDelta = weatherDifference*hourProgression;
+	
+	
+	return behindHourWeather+interpolationDelta;
+}	
+
+function getNextLightning()
+{
+	var weather = getNextWeather();
+	var serverTime = getCurrentServerTime();
+	
+	var lightningOdds = ((0.1+(weather-0.9))/1.5);
+	
+	serverTime=Math.round(serverTime/(1000*1));
+	
+	var random = rnd(serverTime, 0, 1);
+	if (random<=lightningOdds)
+	{
+		var lightLevel = rnd(getCurrentServerTime(), -1.5, 0.8);
+		if (lightLevel<0) lightLevel = 0;
+		return lightLevel;
+	}
+	return 0;
+}
