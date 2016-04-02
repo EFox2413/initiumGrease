cccc56b379099ea8a5be560001aa086b5f18376a
gitolite <EFox2413@gmail.com>
2016-04-02 17:49:51 +0700

    no-refresh.js prototype complete

--- no-refresh.js
+++ no-refresh.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         no-refresh
-// @namespace   https://github.com/EFox2413 
+// @namespace   https://github.com/EFox2413
 // @version      0.1.1
 // @updateURL    https://raw.githubusercontent.com/EFox2413/initiumGrease/master/no-refresh.js
 // @downloadURL    https://raw.githubusercontent.com/EFox2413/initiumGrease/master/no-refresh.js
@@ -12,92 +12,91 @@
 /* jshint -W097 */
 'use strict';
 
-var $ = window.jQuery; 
+var $ = window.jQuery;
 
-doExplore = doFastExplore;
+var buttonRight = $( '.main-button' ).first();
+var buttonLeft = $( '.main-button' ).first().next();
 
-function doFastExplore(ignoreCombatSites)
-{
-    var urlString = "ServletCharacterControl?type=explore_ajax&ignoreCombatSites=" + ignoreCombatSites;
-    var loadPage = "https://www.playinitium.com/main.jsp";
+var linkArr = [];
+linkArr.push(buttonRight.attr( "href"));
+linkArr.push(buttonLeft.attr( "href" ));
 
-    if (ignoreCombatSites == null)
-        ignoreCombatSites = false;
-    showBannerLoadingIcon();
-    // URL contains
-    // {"isComplete":false,"timeLeft":2,"locationName":"Combat site: Hobgoblin Hunter"}
+// disables href redirect for first 2 buttons
+buttonRight.removeAttr( "href" );
+buttonLeft.removeAttr( "href" );
 
-    // longOperation(eventObject, actionUrl, responseFunction, recallFunction)
-    newLongOperation(null, urlString, function(action) {
-        if (action.isComplete)
-        {
-            //fullpageRefresh();
-            $( 'body' ).load(loadPage);
-        }
-        else
-        {
-            var locationName = action.locationName;
-            popupPermanentOverlay_Searching(locationName, window.biome);
+// on click calls the get data function
+buttonRight.click( function() {
+    getData(linkArr[0]);
+});
 
-        }
-    },function() {
-        doFastExplore(ignoreCombatSites, window.biome);
-    });
-}
+buttonLeft.click( function() {
+    getData(linkArr[1]);
+});
 
-var lastLongOpEventObject = null;
-
-function newLongOperation(eventObject, actionUrl, responseFunction, recallFunction)
-{
-    lastLongOpEventObject = eventObject;
-    $.get(actionUrl).done(function(data){
-        if (data.error != undefined)
-        {
-            hideBannerLoadingIcon();
-            popupMessage("System Message", data.error, false);
-            if (data.refresh==true)
-                //fullpageRefresh();
-                $( 'body' ).load(loadPage);
-            return;
-        }
-        if (data.refresh==true)
-        {
-            //fullpageRefresh();
-            $( 'body' ).load(loadPage);
-            return;
-        }
-        if (responseFunction!=null)
-            responseFunction(data);
-
-        if (data.isComplete==false)
-        {
-            if (data.timeLeft>=0)
-            {
-                setTimeout(recallFunction, (data.timeLeft+1)*1000);
-                if (data.timeLeft>=5)
-                    popupPremiumReminder();
+// gets the data from the actionURL
+function getData(actionURL) {
+    $.get(actionURL)
+        .done(function(data) {
+            processData(data);
             }
-        }
-        else
-        {
-            if (data.description!=null)
-                $("#long-operation-complete-text").html(data.description);
-        }
-        lastLongOpEventObject = null;
-    })
-        .fail(function(xhr, textStatus, errorThrown){
-        if (errorThrown=="Internal Server Error")
-            popupMessage(errorThrown, "There was an error when trying to perform the action. Feel free to report this on <a href='http://initium.reddit.com'>/r/initium</a>. A log has been generated.");
-        else
-            popupMessage(errorThrown, "There was an error when trying to perform the action.");
+         );
+}
 
-        lastLongOpEventObject = null;
-    });
+console.log($( '.main-page' ).last().children().html());
+// Gets the hp values and the battle description from the returned data
+function processData(data) {
+    var $data = $(data);
+    var $mainPageDiv = $data.filter( '.main-page' ).last();
+    var $hpObj = $mainPageDiv.find( '.character-display-box' ).children( 'div' ).children( 'div' ).children( 'p' );
 
-    if (eventObject!=null)
-        eventObject.stopPropagation();
+    // gets the new hps
+    var playerHP = $hpObj.first().text();
+    var enemyHP = $hpObj.last().text();
+
+    // gets the new combat text
+    var combatHTML = parseCombatHTML($mainPageDiv.html());
+
+    // if either the player or the enemy is dead call for refresh
+    //   because of the way the HTML works if the enemy HP div is missing
+    //   it will use the same div for both objects
+    if( playerHP === enemyHP ) { fullpageRefresh(); }
+
+    // data is processed, now update the relevant divs
+    updateCombat(playerHP, enemyHP, combatHTML);
+
+    // TODO REMOVE
+    console.log(playerHP + ", " + enemyHP + "\n html: " + combatHTML);
 }
 
 
+// TODO smarter way of updating the hpObjects
+/*
+// this gets the player hp div
+html = html.slice(html.indexOf( "<div style=\"display:inline-block;\">",
+                  html.indexOf( "<div class=\"buff-pane hint"));
 
+// this gets the enemy div
+html = html.slice(html.indexOf( "<div class=\"character-display-box\"\ style=\"float:right",
+                  html.indexOf( "<div calss=\"chat_box"));
+ */
 
+// parses HTML and gets the combat related HTML
+function parseCombatHTML( html ){
+    html = html.slice(html.indexOf( "<p" ));
+
+    console.log( html );
+    return html;
+}
+
+// updates the HTML elements on the current page with the values
+function updateCombat(newPlayerHP, newEnemyHP, combatHTML) {
+    var $hpObj = $( '.character-display-box' ).children( 'div' ).children( 'div' ).children( 'p' );
+    var playerHP = $hpObj.first();
+    var enemyHP = $hpObj.last();
+
+    playerHP.text( newPlayerHP );
+    enemyHP.text( newEnemyHP );
+    $( '.main-page' ).last().append( '<div class="combat-text"> </div>' );
+    $( '.combat-text' ).html( combatHTML );
+}
