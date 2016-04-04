280a5f33d5e7fcb7b79cc6fd2fdce5279d5b354e
gitolite <EFox2413@gmail.com>
2016-04-04 17:45:30 +0700

    Aggregated all javascript to initium-plus.js Next step is to modularize the scripts with a coherent API

--- scripts/icons-script.js
+++ scripts/icons-script.js
@@ -1,13 +1,23 @@
-var $ = window.jQuery; 
+var $ = window.jQuery;
 
+// links to resources
 var mapLink = "https://imgur.com/ZuUibeV";
 var changelogLink = "viewChangelog()";
 
+// filters to make the icons look different
+// TODO make more cross-compatible
 var greenFilter = "-webkit-filter: hue-rotate(50deg)";
 var blueFilter = "-webkit-filter: hue-rotate(100deg)";
- 
-var insert = "<a href=\"" + mapLink + "\"><img id=\"community-map-image\" src=\"images/ui/settings.gif\" style=\"max-height:18px; " + greenFilter + "\"></a>";
-$( '.header-stats' ).append( insert );
 
-insert =  "<a onclick=\"" + changelogLink + "\"><img id=\"community-map-image\" src=\"images/ui/settings.gif\" style=\"max-height:18px; " + blueFilter + "\"></a>";
-$( '.header-stats' ).append( insert );
+// html to be inserted
+var insertHTML = "<a href=\"" + mapLink + "\"><img id=\"community-map-image\"" +
+             "src=\"images/ui/settings.gif\" style=\"max-height:18px; " +
+             greenFilter + "\"></a>";
+
+$( '.header-stats' ).append( insertHTML );
+
+insertHTML = "<a onclick=\"" + changelogLink + "\">" +
+         "<img id=\"community-map-image\" src=\"images/ui/settings.gif\"" +
+         "style=\"max-height:18px; " + blueFilter + "\"></a>";
+
+$( '.header-stats' ).append( insertHTML );
--- scripts/weather-script.js
+++ scripts/weather-script.js
@@ -1,4 +1,4 @@
-var $ = window.jQuery; 
+var $ = window.jQuery;
 
 // Weather ratio from getWeather function in server js file...
 var weatherInt = getWeather();
@@ -11,11 +11,11 @@ function translateToString(wRatio, LRatio) {
         if ( LRatio > 0) {
             return "Stormy";
         } else {
-        return"Rain";
-		}
+            return"Rain";
+        }
     } else if (wRatio <0.5) {
-    	// It's not sunny at night...
-    	if (checkNight()) return "Clear";
+        // It's not sunny at night...
+        if (checkNight()) return "Clear";
         return "Sunny";
     }
     return "N/A";
@@ -26,57 +26,57 @@ $( '.header-location' ).append(  '<span ' + getColor() + '> ' + "       Next: " 
 
 // returns a color for the string based on whether it's night time or day time
 function getColor() {
-	if (checkNight()) return 'style="color:purple"';
-	return 'style="color:yellow"';
+    if (checkNight()) return 'style="color:purple"';
+    return 'style="color:yellow"';
 }
 
 // Weather calculator
 function getNextWeather()
 {
-	var serverTime = getCurrentServerTime();
-	var date = new Date(serverTime);
-	
-	var behindHour = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours()+1);
-	var aheadHour = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours()+2);
-	var behindMs = behindHour.getTime();
-	var aheadMs = aheadHour.getTime();
+    var serverTime = getCurrentServerTime();
+    var date = new Date(serverTime);
 
-	var behindHourWeather = rnd((behindMs/3600000), 0, 1);
-	var aheadHourWeather = rnd((aheadMs/3600000), 0, 1);
-	
-	var weatherDifference = aheadHourWeather-behindHourWeather;
-	
-	var hourProgression = (serverTime-behindHour)/3600000;
-	
-	var interpolationDelta = weatherDifference*hourProgression;
-	
-	
-	return behindHourWeather+interpolationDelta;
-}	
+    var behindHour = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours()+1);
+    var aheadHour = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours()+2);
+    var behindMs = behindHour.getTime();
+    var aheadMs = aheadHour.getTime();
+
+    var behindHourWeather = rnd((behindMs/3600000), 0, 1);
+    var aheadHourWeather = rnd((aheadMs/3600000), 0, 1);
+
+    var weatherDifference = aheadHourWeather-behindHourWeather;
+
+    var hourProgression = (serverTime-behindHour)/3600000;
+
+    var interpolationDelta = weatherDifference*hourProgression;
+
+
+    return behindHourWeather+interpolationDelta;
+}
 
 function getNextLightning()
 {
-	var weather = getNextWeather();
-	var serverTime = getCurrentServerTime();
-	
-	var lightningOdds = ((0.1+(weather-0.9))/1.5);
-	
-	serverTime=Math.round(serverTime/(1000*1));
-	
-	var random = rnd(serverTime, 0, 1);
-	if (random<=lightningOdds)
-	{
-		var lightLevel = rnd(getCurrentServerTime(), -1.5, 0.8);
-		if (lightLevel<0) lightLevel = 0;
-		return lightLevel;
-	}
-	return 0;
+    var weather = getNextWeather();
+    var serverTime = getCurrentServerTime();
+
+    var lightningOdds = ((0.1+(weather-0.9))/1.5);
+
+    serverTime=Math.round(serverTime/(1000*1));
+
+    var random = rnd(serverTime, 0, 1);
+    if (random<=lightningOdds)
+    {
+        var lightLevel = rnd(getCurrentServerTime(), -1.5, 0.8);
+        if (lightLevel<0) lightLevel = 0;
+        return lightLevel;
+    }
+    return 0;
 }
 
 function checkNight() {
-	var randConstant = 318.47133757961783439490445859873;
-	var serverTime = getCurrentServerTime() / (randConstant*60*60*1.5); 
-	var amount = Math.abs(Math.sin(serverTime)) * 3.0 - 1.56;
-	
-	return amount > 1;
+    var randConstant = 318.47133757961783439490445859873;
+    var serverTime = getCurrentServerTime() / (randConstant*60*60*1.5);
+    var amount = Math.abs(Math.sin(serverTime)) * 3.0 - 1.56;
+
+    return amount > 1;
 }
